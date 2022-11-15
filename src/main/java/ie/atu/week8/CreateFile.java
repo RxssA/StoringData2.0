fa9f105b3b0bdad8b528aaa668cdcaacdb547e96
g00401655 <g00401655@atu.ie>
2022-11-15 15:01:07 +0000

    sent data to myFile and stored the old data and data is formatted

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -2,12 +2,30 @@ package ie.atu.week8;
 
 
 import java.io.*;
+import java.util.Scanner;
 
 public class CreateFile {
     public static void main(String[] args) {
         {
-            File myFile = new File("Myfile.txt");
+
+
+
             System.out.println("My File is located at " + myFile.getAbsolutePath());
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Entewr the filename: ");
+            String filename = keyboard.nextLine();
+            Scanner myFile = new Scanner(new  File(filename));
+            String content = "Details to write";
+            try{
+                PrintWriter myWriter = new PrintWriter(myFile);
+
+                myWriter.close();
+
+
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
         }
 
 
