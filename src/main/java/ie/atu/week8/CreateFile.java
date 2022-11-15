5c389e928dd29aa06c55f7ea6962082d3f225268
g00401655 <g00401655@atu.ie>
2022-11-15 13:56:16 +0000

    created new file called myFile

---
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -0,0 +1,15 @@
+package ie.atu.week8;
+
+
+import java.io.*;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        {
+            File myFile = new File("Myfile.txt");
+            System.out.println("My File is located at " + myFile.getAbsolutePath());
+        }
+
+
+    }
+}
