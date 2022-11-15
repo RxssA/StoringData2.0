256bad75349d30a02becf1898972d20bde724420
g00401655 <g00401655@atu.ie>
2022-11-15 14:09:57 +0000

    sent data to myFile

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -8,6 +8,17 @@ public class CreateFile {
         {
             File myFile = new File("Myfile.txt");
             System.out.println("My File is located at " + myFile.getAbsolutePath());
+            String content = "Details to write";
+            try{
+                FileWriter myWriter = new FileWriter(myFile);
+                myWriter.write("hello world 2");
+                myWriter.close();
+
+
+            }
+            catch (IOException e){
+                e.printStackTrace();
+        }
         }
 
 
